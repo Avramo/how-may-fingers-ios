b584a81a1f47955d2dd9d42091be0e809b17022d
elnatan <elnatan@ravtech.co.il>
2020-01-20 16:45:09 +0200

    Initial Commit

---
+++ How Many Fingers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  How Many Fingers
+//
+//  Created by admin on 20/01/2020.
+//  Copyright © 2020 AM Kirsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
